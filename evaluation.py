0fe2b450b1dd333f345032f21a434f34ddbd8008
hiroakitanaka216 <hiroaki-tanaka216@keio.jp>
2021-12-16 21:11:21 +0900

    change comment

--- evaluation.py
+++ evaluation.py
@@ -12,7 +12,7 @@ import seq2seq_GRU
 import train
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epoch', type=int, default=75000)
+parser.add_argument('--epoch', type=int, default=75000) #or default=1
 args = parser.parse_args()
 
 def evaluate(encoder, decoder, sentence, max_length=lang_data.MAX_LENGTH):
