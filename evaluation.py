545eaeb5d0992a8aa4cb8191765602453bf067b3
hiroakitanaka216 <tanas@DESKTOP-3PSRQ6E.localdomain>
2021-12-16 20:56:03 +0900

    first commit

---
+++ evaluation.py
@@ -0,0 +1,158 @@
+from __future__ import unicode_literals, print_function, division
+import random
+import torch
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import argparse
+
+
+import lang_data
+import seq2seq_GRU
+import train
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epoch', type=int, default=75000)
+args = parser.parse_args()
+
+def evaluate(encoder, decoder, sentence, max_length=lang_data.MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = seq2seq_GRU.tensorFromSentence(lang_data.input_lang, sentence) #GRU/CNN
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=seq2seq_GRU.device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[lang_data.SOS_token]], device=seq2seq_GRU.device)  # SOS #GRU/CNN
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == lang_data.EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(lang_data.output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+
+
+######################################################################
+# We can evaluate random sentences from the training set and print out the
+# input, target, and output to make some subjective quality judgements:
+#
+
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(lang_data.pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
+
+
+######################################################################
+# Training and Evaluating
+# =======================
+#
+# With all these helper functions in place (it looks like extra work, but
+# it makes it easier to run multiple experiments) we can actually
+# initialize a network and start training.
+#
+# Remember that the input sentences were heavily filtered. For this small
+# dataset we can use relatively small networks of 256 hidden nodes and a
+# single GRU layer. After about 40 minutes on a MacBook CPU we'll get some
+# reasonable results.
+#
+# .. Note::
+#    If you run this notebook you can train, interrupt the kernel,
+#    evaluate, and continue training later. Comment out the lines where the
+#    encoder and decoder are initialized and run ``trainIters`` again.
+#
+
+hidden_size = 256
+encoder1 = seq2seq_GRU.EncoderRNN(lang_data.input_lang.n_words, hidden_size).to(seq2seq_GRU.device) #GRU/CNN
+attn_decoder1 = seq2seq_GRU.AttnDecoderRNN(hidden_size, lang_data.output_lang.n_words, dropout_p=0.1).to(seq2seq_GRU.device)
+
+train.trainIters(encoder1, attn_decoder1, args.epoch, print_every=5000)
+
+######################################################################
+#
+
+evaluateRandomly(encoder1, attn_decoder1)
+
+
+######################################################################
+# Visualizing Attention
+# ---------------------
+#
+# A useful property of the attention mechanism is its highly interpretable
+# outputs. Because it is used to weight specific encoder outputs of the
+# input sequence, we can imagine looking where the network is focused most
+# at each time step.
+#
+# You could simply run ``plt.matshow(attentions)`` to see attention output
+# displayed as a matrix, with the columns being input steps and rows being
+# output steps:
+#
+
+output_words, attentions = evaluate(
+    encoder1, attn_decoder1, "je suis trop froid .")
+plt.matshow(attentions.numpy())
+
+
+######################################################################
+# For a better viewing experience we will do the extra work of adding axes
+# and labels:
+#
+
+def showAttention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+
+def evaluateAndShowAttention(input_sentence):
+    output_words, attentions = evaluate(
+        encoder1, attn_decoder1, input_sentence)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    showAttention(input_sentence, output_words, attentions)
+
+
+evaluateAndShowAttention("elle a cinq ans de moins que moi .")
+
+evaluateAndShowAttention("elle est trop petit .")
+
+evaluateAndShowAttention("je ne crains pas de mourir .")
+
+evaluateAndShowAttention("c est un jeune directeur plein de talent .")
